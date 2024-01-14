9577a54ff46f1542d431120862107ce6e1b72cf6
Flolo420 <154350744+Flolo420@users.noreply.github.com>
2024-01-14 17:28:07 +0100

    Initialize ref-installer-3

---
+++ ollama/releases.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var github = require('../_common/github.js');
+var owner = 'jmorganca';
+var repo = 'ollama';
+
+module.exports = async function (request) {
+  let all = await github(request, owner, repo);
+
+  let releases = [];
+  for (let rel of all.releases) {
+    // this is a janky, sudo-wantin' .app
+    let isJank = rel.name.startsWith('Ollama-darwin');
+    if (isJank) {
+      continue;
+    }
+
+    let isUniversal = rel.name === 'ollama-darwin';
+    if (isUniversal) {
+      let x64 = Object.assign({ arch: 'x86_64' }, rel);
+      releases.push(x64);
+
+      rel.arch = 'aarch64';
+    }
+    releases.push(rel);
+  }
+  all.releases = releases;
+
+  return all;
+};
+
+if (module === require.main) {
+  module.exports(require('@root/request')).then(function (all) {
+    all = require('../_webi/normalize.js')(all);
+    console.info(JSON.stringify(all));
+    //console.info(JSON.stringify(all, null, 2));
+  });
+}
